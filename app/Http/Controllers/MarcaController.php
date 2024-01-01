0ec48c50129911dde324bc4355cf107e485771c8
Paulo <pauloecomercial367@gmail.com>
2024-01-01 19:20:30 -0300

    :sparkles: Repository para marca

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Storage;
 use App\Models\Marca;
 use Illuminate\Http\Request;
 use Ramsey\Uuid\Type\Integer;
+use App\Repositories\MarcaRepository;
+use Illuminate\Support\Facades\Storage;
 
 class MarcaController extends Controller
 {
@@ -20,24 +21,23 @@ class MarcaController extends Controller
      */
     public function index(Request $request)
     {
-        $marcas = [];
+        $marcaRepository = new MarcaRepository($this->marca);
+
         if ($request->has("atributos_modelos")) {
-            $atributos_modelos = $request->atributos_modelos;
-            $marcas = $this->marca->with('modelos:id,' . $atributos_modelos);
+            $atributos_modelos = 'modelos:id,' . $request->atributos_modelos;
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
         } else {
-            $marcas = $this->marca->with('modelos');
+            $marcaRepository->selectAtributosRegistrosRelacionados("modelos");
         }
-
         if ($request->has("filtro")) {
-            $filtros = explode(";", $request->filtro);
-            foreach ($filtros as $filtro) {
-                $c = explode(":", $request->filtro);
-                $marcas = $marcas->where($c[0], $c[1], $c[2]);
-            }
+            $marcaRepository->filtro($request->filtro);
         }
 
+        if ($request->has("atributos")) {
+            $marcaRepository->selectAtributos($request->atributos);
+        }
 
-        $marcas = $this->marca->with('modelos')->get();
+        $marcas = $marcaRepository->getResultado();
         return response()->json($marcas);
     }
 
